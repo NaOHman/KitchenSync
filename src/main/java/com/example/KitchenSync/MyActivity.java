d57a97f5955d4936a93c42954baf0bbe629c93c1
dgoldste <dgoldste@macalester.edu>
2014-03-07 17:44:51 -0600

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	new file:   res/values/eating_restrictions_array.xml # # Changes not staged for commit: #   (use "git add <file>..." to update what will be committed) #   (use "git checkout -- <file>..." to discard changes in working directory) # #	modified:   .idea/workspace.xml #	modified:   res/layout/list_group.xml #	modified:   res/layout/list_heading.xml #	modified:   res/layout/list_item.xml #	modified:   res/layout/main.xml #	modified:   res/values/eating_restrictions_array.xml #	modified:   src/main/java/com/example/KitchenSync/MyActivity.java # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # #	target/

--- src/main/java/com/example/KitchenSync/MyActivity.java
+++ src/main/java/com/example/KitchenSync/MyActivity.java
@@ -29,7 +29,7 @@ public class MyActivity extends Activity {
     private Weekday weekday;
 
     /**
-     *sfldjflskdjf ;slkjfg;skdjfhg;ljnsCalled when the activity is first created.
+     * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,7 +43,7 @@ public class MyActivity extends Activity {
         int date = calendar.get(Calendar.DATE);
         int day = calendar.get(Calendar.DAY_OF_WEEK);
         setDayValues(day);
-        dateDisplay.setText("Today is " + dayString + ", " + month + " / " + date);
+        dateDisplay.setText("Today is " + dayString + ", " + month + " / " + date);  //TODO date comes up as invalid
 
         //sets up Day selector spinner
         selectDay = (Spinner) findViewById(R.id.daySelectorSpinner);
