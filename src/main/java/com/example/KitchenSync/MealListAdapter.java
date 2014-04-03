e9b7b0e4cb1c3713bdca62c7e284e4cc056cdc6d
jeffrey <JLyman@macalester.edu>
2014-04-02 19:53:41 -0500

    Now pulling data from Heroku Server!

--- src/main/java/com/example/KitchenSync/MealListAdapter.java
+++ src/main/java/com/example/KitchenSync/MealListAdapter.java
@@ -2,6 +2,7 @@ package com.example.KitchenSync;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- src/main/java/com/example/KitchenSync/MyActivity.java
+++ src/main/java/com/example/KitchenSync/MyActivity.java
@@ -10,10 +10,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.*;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
+import com.google.gson.Gson;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.Calendar;
+import java.io.InputStream;
 
 public class MyActivity extends Activity {
     private MealListAdapter listAdapter;
@@ -90,7 +97,7 @@ public class MyActivity extends Activity {
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected())
-            new WeekDataFetcher().execute(getString(R.string.cafe_url));
+            new WeekDataFetcher().execute(getString(R.string.server_url));
     }
     private void setDayValues(int day){
         Log.d("MyActivity", "Setting Day value = " + day);
@@ -147,8 +154,15 @@ public class MyActivity extends Activity {
         @Override
         protected Week doInBackground(final String... args) {
             try {
-                Document doc = Jsoup.connect(args[0]).get();
-                Week week = new Week(doc);
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                URI server = new URI(args[0]);
+                request.setURI(server);
+                HttpResponse response = httpClient.execute(request);
+                InputStream inputStream = response.getEntity().getContent();
+                String json = convertIStoText(inputStream);
+                Gson gson = new Gson();
+                Week week = gson.fromJson(json, Week.class);
                 return week;
             } catch (Exception e) {
                 return null;
@@ -162,5 +176,18 @@ public class MyActivity extends Activity {
             if (week !=null)
                 setListData(week);
         }
+    }
+    public String convertIStoText(InputStream iS){
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(iS));
+        String line;
+        String result = "";
+        try {
+            while((line = bufferedReader.readLine()) != null)
+                result += line;
+            iS.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
